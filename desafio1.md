2acc7ee371de55c736e4aa2e1cffe70f24282632
Sammuell3 <samuelsongsytlbry@gmail.com>
2023-11-12 00:48:24 -0300

    Desafio concluido

---
+++ desafio1.md
@@ -0,0 +1,24 @@
+# Desafio Classificador de nivel de Herói
+
+** O que deve ser utilizado **
+- Variáveis
+- Operadores
+- Laços de repetição 
+- Estruturas de decisões
+
+## Objetivo
+
+Crie uma variaǘel para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mesagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata Ouro
+Se XP for enter 6.001 e 7.000 = Platina Diamante
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000 = Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+## Saída 
+
+Ao final deve se exibir uma menssagem:
+"O Herói de nome **{nome}**está no nivel de **{nivel}**"
--- index.js
+++ index.js
@@ -1,1 +1,153 @@
-afdwda
+let nome = "Dev_samurai";
+let xp = 10001;
+let ferro = xp <= 1000;
+let bronze = xp >= 1001 && xp <= 2000;
+let prata = xp >= 2001 && xp <= 5000;
+let ouro = xp >= 5001 && xp <= 7000;
+let platina = xp >= 7001 && xp <= 8000;
+let ascendente = xp >= 8001 && xp <= 9000;
+let imortal = xp >= 9001 && xp <= 10000;
+let radiante = xp >= 10001;
+
+//Desafio feito com estrutura de condição
+
+// if (ferro) {
+//     console.log(`O Herói de nome ${nome} está no nível de Ferro`);
+
+// } else if (bronze) {
+//     console.log(`O Herói de nome ${nome} está no nível de Bronze`);
+
+// } else if (prata) {
+//     console.log(`O Herói de nome ${nome} está no nível de Prata`);
+
+// } else if (ouro) {
+//     console.log(`O Herói de nome ${nome} está no nível de Ouro`);
+
+// } else if (platina) {
+//     console.log(`O Herói de nome ${nome} está no nível de Platina`);
+
+// } else if (ascendente) {
+//     console.log(`O Herói de nome ${nome} está no nível de Ascendente`);
+
+// } else if (imortal) {
+//     console.log(`O Herói de nome ${nome} está no nível de Imortal`);
+
+// } else if (radiante) {
+//     console.log(`O Herói de nome ${nome} está no nível de Radiante`);
+
+// } else {
+//     console.log("Nível não definido");
+// }
+
+
+// Switch case
+
+// switch (true) {
+//     case ferro:
+//         console.log(`O Herói de nome ${nome} está no nível de Ferro`)
+//         break;
+//     case bronze:
+//         console.log(`O Herói de nome ${nome} está no nível de Bronze`)
+//         break;
+//     case prata:
+//         console.log(`O Herói de nome ${nome} está no nível de Prata`)
+//         break;
+//     case ouro:
+//         console.log(`O Herói de nome ${nome} está no nível de Ouro`)
+//         break;
+//     case platina:
+//         console.log(`O Herói de nome ${nome} está no nível de Platina`)
+//         break;
+//     case ascendente:
+//         console.log(`O Herói de nome ${nome} está no nível de Ascendente`)
+//         break;
+//     case imortal:
+//         console.log(`O Herói de nome ${nome} está no nível de Imortal`)
+//         break;
+//     case radiante:
+//         console.log(`O Herói de nome ${nome} está no nível de Radiante`)
+//         break;
+//     default:
+//         console.log("Nível não definido");
+// }
+
+// while
+
+// while (true) {
+//     if (ferro) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ferro`)
+//         break;
+//     } else if (bronze) {
+//         console.log(`O Herói de nome ${nome} está no nível de Bronze`)
+//         break;
+//     } else if (prata) {
+//         console.log(`O Herói de nome ${nome} está no nível de Prata`)
+//         break;
+//     } else if (ouro) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ouro`)
+//         break;
+//     } else if (platina) {
+//         console.log(`O Herói de nome ${nome} está no nível de Platina`)
+//         break;
+//     } else if (ascendente) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ascendente`)
+//         break;
+//     } else if (imortal) {
+//         console.log(`O Herói de nome ${nome} está no nível de Imortal`)
+//         break;
+//     } else if (radiante) {
+//         console.log(`O Herói de nome ${nome} está no nível de Radiante`)
+//         break;
+//     } else {
+//         console.log("Nível não definido")
+//         break;
+//     }
+// }
+
+//Do while
+
+// do {
+//     if (ferro) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ferro`);
+//         break;
+//     } else if (bronze) {
+//         console.log(`O Herói de nome ${nome} está no nível de Bronze`);
+//         break;
+//     } else if (prata) {
+//         console.log(`O Herói de nome ${nome} está no nível de Prata`);
+//         break;
+//     } else if (ouro) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ouro`);
+//         break;
+//     } else if (platina) {
+//         console.log(`O Herói de nome ${nome} está no nível de Platina`);
+//         break;
+//     } else if (ascendente) {
+//         console.log(`O Herói de nome ${nome} está no nível de Ascendente`);
+//         break;
+//     } else if (imortal) {
+//         console.log(`O Herói de nome ${nome} está no nível de Imortal`);
+//         break;
+//     } else if (radiante) {
+//         console.log(`O Herói de nome ${nome} está no nível de Radiante`);
+//         break;
+//     } else {
+//         console.log("Nível não definido");
+//         break;
+//     }
+// } while (true);
+
+// for each
+
+const niveis = [ferro, bronze, prata, ouro, platina, ascendente, imortal, radiante];
+
+for (const nivel of niveis) {
+    if (nivel) {
+        console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+        break;
+    }
+}
+
+//Se nenhuma condiçao for encontrada
+console.log("Nível não definido");
+
